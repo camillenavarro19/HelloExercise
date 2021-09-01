1ccaaf1c656a4e741ce1afe2a5e862f560bcfe89
camillenavarro19 <55165346+camillenavarro19@users.noreply.github.com>
2021-09-01 18:04:52 -0400

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = "Camille";
+        // String input = keyboard.nextLine();
+        System.out.println("Hello, " + name);
+    }
+}
